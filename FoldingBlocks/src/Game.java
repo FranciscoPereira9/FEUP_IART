b6b665f0698cdcdf9a8b82f72cdba7d82260d8d5
BernardoCMoreira <bernardo1998.99@sapo.pt>
2020-02-29 15:48:20 +0000

    Level2 added

--- FoldingBlocks/src/Game.java
+++ FoldingBlocks/src/Game.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,11 +10,14 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import Levels.Level1;
+import Levels.Level2;
 
 @SuppressWarnings("serial")
 public class Game extends JPanel implements KeyListener, ActionListener {
 	
 	private Timer timer;
+	private Level2 l2 = new Level2();
+	private int numberOfPlays = 0;
 	
 	public Game() {
 		addKeyListener(this);
@@ -21,7 +25,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		setFocusTraversalKeysEnabled(false);
 		timer = new Timer(8, this);
 		timer.start();
-		
+
 	}
 	
 	public void paint(Graphics g) {
@@ -31,8 +35,13 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		g.fillRect(1,1,692,592);
 		
 		//Game Level
-		new Level1(g);
-			
+		l2.drawLevel2(g);
+		
+		//draw time
+		g.setColor(Color.lightGray);
+		g.setFont(new Font("serif", Font.BOLD, 25));
+		g.drawString(""+numberOfPlays, 30,30);
+	
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
@@ -41,20 +50,25 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) {
+		//Aumenta o numero de jogadas;
+		numberOfPlays++;
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			//Do some movement;
+		}
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+
 		
 	}
 
---
+++ FoldingBlocks/src/Levels/Level2.java
@@ -0,0 +1,54 @@
+package Levels;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Level2 extends JPanel {
+	
+	public Level2() {
+		
+	}
+	
+	public void drawLevel2(Graphics g) {
+		g.setColor(Color.white);
+		
+		//Paint game board
+		g.fillRect(100, 100, 95, 95);
+		g.fillRect(200, 100, 95, 95);
+		g.fillRect(300, 100, 95, 95);
+		g.fillRect(400, 100, 95, 95);
+		g.fillRect(500, 100, 95, 95);
+		
+		g.fillRect(100, 200, 95, 95);
+		g.fillRect(200, 200, 95, 95);
+		g.fillRect(300, 200, 95, 95);
+		g.fillRect(400, 200, 95, 95);
+		g.fillRect(500, 200, 95, 95);
+		
+		g.fillRect(100, 300, 95, 95);
+		g.fillRect(200, 300, 95, 95);
+		g.fillRect(300, 300, 95, 95);
+		g.fillRect(400, 300, 95, 95);
+		g.fillRect(500, 300, 95, 95);
+		
+		g.fillRect(100, 400, 95, 95);
+		g.fillRect(200, 400, 95, 95);
+		g.fillRect(300, 400, 95, 95);
+		g.fillRect(400, 400, 95, 95);
+		g.fillRect(500, 400, 95, 95);
+		
+		//Paint pieces to move
+		g.setColor(Color.CYAN);
+		g.fillRect(100, 100, 95, 95);
+		
+		g.setColor(Color.MAGENTA);
+		g.fillRect(500, 400, 95, 95);
+	}
+	
+	public int getNumberPieces() {
+		final int number = 2;
+		return number;
+	}
+}
