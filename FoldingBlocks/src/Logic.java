2c2511c41b548fe15414f52827f135827209b704
Francisco Pereira <francisco@DESKTOP-RNTM3PU.localdomain>
2020-03-12 16:49:47 +0000

    move_validation function in class Logic

--- FoldingBlocks/src/Logic.java
+++ FoldingBlocks/src/Logic.java
@@ -6,7 +6,11 @@ public class Logic {
 		
 	public int x_axis;
 	public int y_axis;
-	
+/*
+ * Gets the coordinates of the folding axis related to the piece that's being played. Returns [x_axis,y_axis].
+ * x_axis is a vertical axis
+ * y_axis is a horizontal axis	
+ */
 public int[] get_axis(int[][] mat, int move, int ID_block) {
 
 		
@@ -117,7 +121,11 @@ public int[] get_axis(int[][] mat, int move, int ID_block) {
         return axis;
 
 	}
-	
+
+
+/*
+ * Folds the piece meant to be played and returns the board updated with the play
+ */
 public int[][] fold(int[][] mat, int move, int ID_block) {
 		
 		int axis[]=this.get_axis(mat, move, ID_block);
@@ -135,7 +143,7 @@ public int[][] fold(int[][] mat, int move, int ID_block) {
     				if(mat[x][y]==ID_block) {
     					dist=(y-axis[1])+1;
     					bloco_novo=axis[1]-dist;
-    					if(bloco_novo < 0 || bloco_novo >= mat[x].length || mat[x][bloco_novo] != 0) {
+    					if(this.move_validation(mat,bloco_novo,move,x,y) == false) {
     						mat_aux=mat;
     						return mat;
     					}
@@ -148,7 +156,7 @@ public int[][] fold(int[][] mat, int move, int ID_block) {
     				if(mat[x][y]==ID_block) {
     					dist=(axis[1]-y)+1;
     					bloco_novo=axis[1]+dist;
-    					if(bloco_novo < 0 || bloco_novo >= mat[x].length || mat[x][bloco_novo] != 0) {
+    					if(this.move_validation(mat,bloco_novo,move,x,y) == false) {
     							mat_aux=mat;
     							return mat;
     						}
@@ -160,7 +168,7 @@ public int[][] fold(int[][] mat, int move, int ID_block) {
     				if(mat[x][y]==ID_block) {
     					dist=(x-axis[0])+1;
     					bloco_novo=axis[0]-dist;
-    					if(bloco_novo < 0 || bloco_novo >= mat.length || mat[bloco_novo][y] != 0) {
+    					if(this.move_validation(mat,bloco_novo,move,x,y) == false) {
     						mat_aux=mat;
     						return mat;
     					}
@@ -172,7 +180,7 @@ public int[][] fold(int[][] mat, int move, int ID_block) {
     				if(mat[x][y]==ID_block) {
     					dist=(axis[0]-x)+1;
     					bloco_novo=axis[0]+dist;
-    					if(bloco_novo < 0 || bloco_novo >= mat.length || mat[bloco_novo][y] != 0) {
+    					if(this.move_validation(mat, bloco_novo, move, x, y) == false) {
     						mat_aux=mat;
     						return mat;
     					}
@@ -199,6 +207,31 @@ public int[][] fold(int[][] mat, int move, int ID_block) {
 
 	}
 
+
+/*
+ * If move is valid the function will return a boolean true. Otherwise returns false.
+ */
+public boolean move_validation(int[][] mat, int  bloco_novo, int move, int x, int y) {
+	
+	switch(move) {
+		case 1: 
+		case 2:
+			if(bloco_novo < 0 || bloco_novo >= mat[x].length || mat[x][bloco_novo] != 0) return false;
+			else return true;
+		case 3:
+		case 4:
+			if(bloco_novo < 0 || bloco_novo >= mat.length || mat[bloco_novo][y] != 0) return false;
+			else return true;
+		default:
+			return false;
+	}
+	
+}
+
+/*
+ * Verifies if the board is full.
+ * Returns true if the board is full and false if it's not.
+ */
 public boolean isBoardFull(int[][] board){
 		for(int i=0; i<board[0].length;i++){
 			for(int j=0; j<board.length; j++){
@@ -208,6 +241,9 @@ public boolean isBoardFull(int[][] board){
 		return true;
 	}
 
+/*
+ * Prints in the terminal a matrix of the board. Auxiliary function for verifications.
+ */
 public void print2D(int mat[][]) { 
     // Loop through all rows 
     for (int[] row : mat) { 
@@ -220,7 +256,9 @@ public void print2D(int mat[][]) {
 
 }
 
-
+/*
+ * Auxiliary function that allows to have an auxiliary double array in the fold function.
+ */
 public static int[][] cloneArray(int[][] src) {
     int length = src.length;
     int[][] target = new int[length][src[0].length];
@@ -229,5 +267,6 @@ public static int[][] cloneArray(int[][] src) {
     }
     return target;
 }
+
 
 }
