a98cfa5a77ad15496dfd6e03fcbbdd775d1cb4ab
BernardoCMoreira <up201604014@fe.up.pt>
2020-03-15 22:58:36 +0000

    Main bug fixed

--- FoldingBlocks/src/Algoritmo.java
+++ FoldingBlocks/src/Algoritmo.java
@@ -35,7 +35,7 @@ public class Algoritmo {
 	public boolean verifyFinalState(int[][]board){
 		for (int i=0; i<board.length; i++){
 			for(int j=0;j<board[0].length; j++){
-				if(board[j][i] == 0) return false;
+				if(board[i][j] == 0) return false;
 			}
 		} 
 		return true;
